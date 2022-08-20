d2c6e0eddea78c5fae2536725e105258f4b614cd
XDforLOL <artur1881@gmail.com>
2022-08-21 01:42:46 +0300

    Clearing stuff

--- abra_restapi_messenger/urls.py
+++ abra_restapi_messenger/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from abra_restapi_messenger import views
 
 urlpatterns = [
+    path('', views.api_home),
     path('admin/', admin.site.urls),
     path('', include('messenger_api.urls'))
 
--- abra_restapi_messenger/views.py
+++ abra_restapi_messenger/views.py
@@ -4,15 +4,6 @@ from rest_framework.response import Response
 from messenger_api.models import Message
 from messenger_api.serializers import MessageSerializer
 
-@api_view(['POST'])
+@api_view(['get'])
 def api_home(request, *args, **kwargs):
-    """
-    DRF API View
-    """
-    serializer = MessageSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        # instance = serializer.save()
-        # instance = form.save()
-        print(serializer.data)
-        return Response(serializer.data)
-    return Response({"invalid": "not good data"}, status=400)
+    return Response({"Instruction": "For Instruction on api use refer to the postman file and change the localhost to heroku endpoint and login with the given user and password"}, status=200)
