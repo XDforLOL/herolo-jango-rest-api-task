d1fbc28faa2f3b4016e0691c5f56de7a31a4a420
XDforLOL <artur1881@gmail.com>
2022-08-21 01:45:28 +0300

    Clearing stuff

--- abra_restapi_messenger/views.py
+++ abra_restapi_messenger/views.py
@@ -1,8 +1,6 @@
-from django.forms.models import model_to_dict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from messenger_api.models import Message
-from messenger_api.serializers import MessageSerializer
+
 
 @api_view(['get'])
 def api_home(request, *args, **kwargs):
